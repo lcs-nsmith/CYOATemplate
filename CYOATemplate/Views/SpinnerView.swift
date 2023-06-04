b7d1aa9896abe908ebce70e56f7f6fd4d6e51a70
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2023-06-04 16:26:04 -0400

    Implemented false probabilities (actual probabilities don't need to match the visual ones)

--- CYOATemplate/Views/SpinnerView.swift
+++ CYOATemplate/Views/SpinnerView.swift
@@ -9,16 +9,29 @@ import SwiftUI
 
 struct SpinnerView: View {
     var size: Double
-    var percentage1: Double
-    var percentage2: Double
+    var visualPercentage1: Int
+    var visualPercentage2: Int
+    var actualPercentage1: Int
+    var actualPercentage2: Int
+    @Binding var outcome: Int
     var totalRotation: Double {
-        return Double(Int.random(in: 3...5)) + Double(Int.random(in: 0...99)) / 100 + 0.005
+        let result = Int.random(in: 0...99)
+        if result < actualPercentage1 {
+            outcome = 1
+            return Double(Int.random(in: 3...5)) + Double(Int.random(in: 0..<visualPercentage1)) / 100 + 0.005
+        }
+        if result < actualPercentage1 + actualPercentage2 {
+            outcome = 2
+            return Double(Int.random(in: 3...5)) + Double(Int.random(in: visualPercentage1..<visualPercentage1 + visualPercentage2)) / 100 + 0.005
+        }
+        outcome = 3
+        return Double(Int.random(in: 3...5)) + Double(Int.random(in: visualPercentage1 + visualPercentage2...99)) / 100 + 0.005
     }
     @State var currentRotation = 0.0
     var body: some View {
         ZStack {
             Circle()
-                .trim(from: 0, to: percentage1 / 100)
+                .trim(from: 0, to: Double(visualPercentage1) / 100)
                 .stroke(Color(hue: 0,
                               saturation: 1.0,
                               brightness: 1.0),
@@ -26,14 +39,14 @@ struct SpinnerView: View {
                 .frame(width: size, height: size)
                 
             Circle()
-                .trim(from: percentage1 / 100, to: (percentage1 + percentage2) / 100)
+                .trim(from: Double(visualPercentage1) / 100, to: Double(visualPercentage1 + visualPercentage2) / 100)
                 .stroke(Color(hue: 1/6,
                             saturation: 1.0,
                             brightness: 1.0),
                         lineWidth: size)
                 .frame(width: size, height: size)
             Circle()
-                .trim(from: (percentage1 + percentage2) / 100, to: 1)
+                .trim(from: Double(visualPercentage1 + visualPercentage2) / 100, to: 1)
                 .stroke(Color(hue: 1/3,
                             saturation: 1.0,
                             brightness: 1.0),
@@ -54,7 +67,10 @@ struct SpinnerView: View {
 struct SpinnerView_Previews: PreviewProvider {
     static var previews: some View {
         SpinnerView(size: 100,
-                    percentage1: 55,
-                    percentage2: 25)
+                    visualPercentage1: 55,
+                    visualPercentage2: 25,
+                    actualPercentage1: 90,
+                    actualPercentage2: 5,
+                    outcome: .constant(1))
     }
 }
